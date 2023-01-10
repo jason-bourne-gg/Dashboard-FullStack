af8adfe0b7cc54471eab6bffd23a68f84cf0a168
jason-bourne-gg <car10@iitbbs.ac.in>
2023-01-10 15:47:14 +0530

    new code

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managmentRoutes from "./routes/managment.js";
+import salesRoutes from "./routes/sales.js";
+
 
 /* Setting up configurations */
 dotenv.config();
@@ -17,3 +22,8 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded ({extended: false}));
 
+// Routes
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/managment", managmentRoutes);
+app.use("/sales", salesRoutes);
